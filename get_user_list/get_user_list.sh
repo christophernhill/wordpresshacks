e551b7e46e6aeec7493e7e86bbca29d5e25c054d
christophernhill <cnh@mit.edu>
2017-08-04 10:46:46 -0400

    Update get_user_list.sh

--- get_user_list/get_user_list.sh
+++ get_user_list/get_user_list.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-WP_INSTANCE=foo
+
+# Set WP_INSTANCE to your wordpress site name
+if [ x${WP_INSTANCE} == x ]; then
+ WP_INSTANCE=foo
+fi
 WP_ROOT=/var/www/html
 WP_CONFIG=${WP_ROOT}/${WP_INSTANCE}/wp-config.php
 
+# Get key parameters
 grep DB_NAME ${WP_CONFIG}
